20e13268fd1582c83364e31f8de162f9c3f1fbf4
elias-sosa <eliasasosa64@gmail.com>
2026-06-04 12:08:40 -0300

    Mundo hola

--- Interfaces/ISistema.h
+++ Interfaces/ISistema.h
@@ -1,5 +1,6 @@
 #ifndef ISISTEMA_H
 #define ISISTEMA_H
+using namespace std;
 
 #include <string>
 #include "../Clases/Enums.h"
@@ -7,35 +8,25 @@
 
 class ICollection;
 
-class ISistema {
+class ISistema
+{
 public:
-    virtual bool existeUsuario(std::string nickname) const = 0;
+    virtual bool existeUsuario(string nickname) = 0;
 
-    virtual Status altaCliente(std::string nickname, std::string contrasenia, std::string nombre,
-                               std::string email, std::string apellido, std::string documento) = 0;
-    virtual Status altaPropietario(std::string nickname, std::string contrasenia, std::string nombre,
-                                   std::string email, std::string cuentaBancaria,
-                                   std::string telefono) = 0;
-    virtual Status altaInmobiliaria(std::string nickname, std::string contrasenia, std::string nombre,
-                                    std::string email, std::string direccion, std::string telefono,
-                                    std::string url) = 0;
+    virtual Status altaCliente(string nickname, string apellido, string documento) = 0;
+    virtual Status altaPropietario(string nickname, string contrasenia, string nombre, string email, string cuentaBancaria, string telefono) = 0;
+    virtual Status altaInmobiliaria(string nickname, string contrasenia, string nombre, string email, string direccion, string telefono, string url) = 0;
 
-    virtual int agregarCasa(std::string nicknamePropietario, std::string direccion,
-                            std::string numPuerta, int superficie, int anioConstruccion,
-                            bool esHorizontal, TipoTecho techo) = 0;
-    virtual int agregarApartamento(std::string nicknamePropietario, std::string direccion,
-                                   std::string numPuerta, int superficie, int anioConstruccion,
-                                   int numPiso, bool tieneAscensor, double gastosComunes) = 0;
+    virtual int agregarCasa(string nicknamePropietario, string direccion, string numPuerta, int superficie, int anioConstruccion, bool esHorizontal, TipoTecho techo) = 0;
+    virtual int agregarApartamento(string nicknamePropietario, string direccion, string numPuerta, int superficie, int anioConstruccion, int numPiso, bool tieneAscensor, double gastosComunes) = 0;
 
     virtual ICollection *listarInmobiliarias() = 0;
-    virtual ICollection *listarInmuebles(std::string nickname) = 0;
-    virtual ICollection *listarInmueblesAdministrados(std::string nickname) = 0;
+    virtual ICollection *listarInmuebles(string nickname) = 0;
+    virtual ICollection *listarInmueblesAdministrados(string nickname) = 0;
     virtual Status administrarInmueble(int idInm) = 0;
 
-    virtual Status altaPublicacion(int idInm, TipoPublicacion tipo, std::string texto,
-                                   double precio) = 0;
-    virtual ICollection *listarPublicaciones(TipoPublicacion tipo, double min, double max,
-                                             TipoInteres interes) = 0;
+    virtual Status altaPublicacion(int idInm, TipoPublicacion tipo, string texto, double precio) = 0;
+    virtual ICollection *listarPublicaciones(TipoPublicacion tipo, double min, double max, TipoInteres interes) = 0;
     virtual DTDetalleInmueble consultarDetalleInmueble(int idPub) = 0;
 
     virtual ICollection *listarInmuebles() = 0;
